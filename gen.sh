48694a4e4d675365d128d42c34bfbf7816ba80af
kreutz <diegokreutz@unipampa.edu.br>
2024-12-11 19:48:06 -0300

    last updates done at Wed Dec 11 19:48:06 -03 2024

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@ while read NOME
 do
     echo "<div class=\"team-member\">"
     echo "    <div class=\"circle-double-bordered\">"
-    echo "        <img src=\"cdn/img/team/no_img.jpg\" alt=\"$NOME\">"
+    echo "        <img src=\"img/team/no_img.jpg\" alt=\"$NOME\">"
     echo "    </div>"
     echo "    <p>$NOME<br>Colaborador</p>"
     echo "</div>"
